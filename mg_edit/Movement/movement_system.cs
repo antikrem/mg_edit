bf21ba222558b46cc9139a82979fc387be7793cb
Chalinda Dinusha Rodrigo <chalindarodrigo@gmail.com>
2020-04-05 01:23:20 +1000

    Recapsulated mvoement state to seperate class

---
+++ mg_edit/Movement/movement_system.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mg_edit.Movement
+{
+    // Encapsulates updates in the 
+    class MovementSystem
+    {
+        // A hard cap on number of positions, to avoid infinite loops
+        private const int MAXIMUM_POSITION_COUNT = 100000;
+
+        // Variables related to entity movement
+
+        // Starting position
+        private (double, double) startingPosition = (0, 0);
+
+        // Starting velocity cartesian
+        private (double, double) velocityCartesian = (0, 0);
+
+        // Starting acceleration cartesian
+        private (double, double) accelerationCartesian = (0, 0);
+
+        // Starting velocity polar
+        private (double, double) velocityPolar = (0, 0);
+
+        // Sets starting position
+        public void SetPosition(double x, double y)
+        {
+            startingPosition = (x, y);
+        }
+
+        // Sets cartesian velocity
+        public void SetVelocity(double x, double y)
+        {
+            velocityCartesian = (x, y);
+        }
+
+        // Sets starting polar components
+        public void SetPolarSpeed(double mag)
+        {
+            this.velocityPolar = (mag, this.velocityPolar.Item2);
+        }
+
+        // Sets starting polar components
+        public void SetPolarAngle(double ang)
+        {
+            this.velocityPolar = (this.velocityPolar.Item1, ang);
+        }
+
+
+
+        // Computes all positions and returns
+        public List<(double, double)> GetPositions() {
+            List<(double, double)> positions = new List<(double, double)>();
+
+            // Velocity as cartesian
+            var (velX, velY) = velocityCartesian;
+
+            // Acceleration as cartesian
+            var (AcelX, AcelY) = accelerationCartesian;
+
+            // Velocity as polar
+            var (mag, ang) = velocityPolar;
+
+            // Running x,y position
+            var (x, y) = startingPosition;
+            positions.Add(startingPosition);
+
+            // Iterate while in gamepace and below tick max
+            for (int tick = 0;
+                (tick < MAXIMUM_POSITION_COUNT) && GameState.IsInGameSpace(x, y);
+                tick++)
+            {
+                x = x + velX;
+                y = y + velY;
+
+                positions.Add((x, y));
+            }
+
+            return positions;
+        }
+    }
+}
--- mg_edit/entity.cs
+++ mg_edit/entity.cs
@@ -3,29 +3,21 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using mg_edit.Movement;
 
 namespace mg_edit
 {
     // Represents an enemy entity in the game
     class Entity
     {
-        // A hard cap on number of positions, to avoid infinite loops
-        private const int MAXIMUM_POSITION_COUNT = 100000;
-
         // Spawning tick
         private int spawningTick = 0;
 
-        // Starting position
-        private (double, double) startingPosition = (0,0);
+        // Movement object to control movement state
+        MovementSystem movementSystem = new MovementSystem();
 
         // List of points specifying position
         private List<(double, double)> positions = new List<(double, double)>();
-
-        // Starting velocity
-        private (double, double) startingVelocity = (0, 0);
-        // starting speed and angle
-        private double speed;
-        private double angle;
 
         // Constructs with known starting tick
         public Entity(int spawningTick)
@@ -36,53 +28,32 @@ namespace mg_edit
         // Sets starting position
         public void SetPosition(double x, double y)
         {
-            startingPosition = (x, y);
+            movementSystem.SetPosition(x, y);
         }
 
         // Sets starting velocity
         public void SetVelocity(double x, double y)
         {
-            this.startingVelocity = (x, y);
+            movementSystem.SetVelocity(x, y);
         }
 
         // Sets starting polar components
         public void SetPolarSpeed(double mag)
         {
-            this.speed = mag;
+            movementSystem.SetPolarSpeed(mag);
         }
 
         // Sets starting polar components
         public void SetPolarAngle(double ang)
         {
-            this.angle = ang;
+            movementSystem.SetPolarAngle(ang);
         }
 
         // Computes all positions this entity takes
-        public void UpdatePositions()
+        public List<(double, double)> UpdatePositions()
         {
-            // Velocity as cartesian
-            var (velX, velY) = startingVelocity;
-
-            // Acceleration as cartesian
-            var (AcelX, AcelY) = startingVelocity;
-
-            // Velocity as polar
-            var (mag, ang) = (speed, angle);
-
-            // Running x,y position
-            var (x, y) = startingPosition;
-            positions.Add(startingPosition);
-
-            // Iterate while in gamepace and below tick max
-            for (int tick = 0; 
-                (tick < MAXIMUM_POSITION_COUNT) && GameState.IsInGameSpace(x, y);
-                tick++)
-            {
-                x = x + velX;
-                y = y + velY;
-
-                positions.Add((x, y));
-            }
+            positions = movementSystem.GetPositions();
+            return positions;
         }
 
         // Gets all positions for entity
