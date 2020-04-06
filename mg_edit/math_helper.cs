78510df8f2fb8fd6934f1cb9d771e7eb852f4d1f
Chalinda Dinusha Rodrigo <chalindarodrigo@gmail.com>
2020-04-07 03:55:35 +1000

    Added math helper static class

---
+++ mg_edit/math_helper.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mg_edit
+{
+    static class MathHelper
+    {
+        // Applies a generic clamp
+        static public T Clamp<T>(T value, T min, T max) where T : IComparable<T>
+        {
+            if (value.CompareTo(min) < 0)
+                return min;
+            else if (value.CompareTo(max) > 0)
+                return max;
+            else
+                return value;
+        }
+
+        // Calculates the cartesian fr
+        static public (double, double) ToCartesian(double magnitude, double angle)
+        {
+            return (magnitude * Math.Cos((Math.PI / 180) * angle), magnitude * Math.Sin((Math.PI / 180) * angle));
+        }
+    }
+}
