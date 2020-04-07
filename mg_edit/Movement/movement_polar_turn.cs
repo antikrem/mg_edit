af6ae68f2cde60f4ca3dd342a0188c347b5e1e88
Chalinda Dinusha Rodrigo <chalindarodrigo@gmail.com>
2020-04-08 05:30:53 +1000

    Added polar turn movement command

---
+++ mg_edit/Movement/movement_polar_turn.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mg_edit.Movement
+{
+    class MovementPolarTurn : MovementQuanta
+    {
+        private readonly double rate;
+        private readonly int duration;
+
+        // Constructor for a MovementPolarTurn
+        public MovementPolarTurn(double rate, int duration)
+        {
+            this.rate = rate;
+            this.duration = duration;
+        } 
+
+        // Check tick is within duration
+        public override bool IsExecuting(int tick)
+        {
+            return tick <= duration;
+        }
+
+        // Update movement system parameters
+        public override void UpdateExecution(int tick, MovementState movementState) 
+        {
+            if (tick == 0)
+            {
+                movementState.AngleChange = rate;
+            }
+            else if (tick == duration)
+            {
+                movementState.AngleChange = 0;
+            }
+        }
+    }
+}
